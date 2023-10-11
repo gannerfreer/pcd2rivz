dbaa8e8a98cffc5f556c9c642bc5dfdd7f577563
Gray <g-ray@qq.com>
2023-10-11 11:45:32 +0800

    使用gflag设置参数

--- src/pcd_to_rviz/src/pcd_to_rviz.cpp
+++ src/pcd_to_rviz/src/pcd_to_rviz.cpp
@@ -7,8 +7,11 @@
 //which contains the required definitions to load and store point clouds to PCD and other file formats.
  
 #include<iostream>
+#include <pcl/common/common.h>
 #include<pcl/visualization/cloud_viewer.h>
+#include <glog/logging.h>
 
+DEFINE_string(txt_path, "/home/gray/Slam/1-competition/output/map/map.pcd", "PCD文件路径");
 
 void viewerOneOff(pcl::visualization::PCLVisualizer& viewer)
 {
@@ -19,16 +22,22 @@ void viewerOneOff(pcl::visualization::PCLVisualizer& viewer)
 
 main (int argc, char **argv)
 {
+    google::InitGoogleLogging(argv[0]);
+    FLAGS_stderrthreshold = google::INFO;
+    FLAGS_colorlogtostderr = true;
+    google::ParseCommandLineFlags(&argc, &argv, true);
+
+
   ros::init (argc, argv, "UandBdetect");
   ros::NodeHandle nh;
   ros::Publisher pcl_pub = nh.advertise<sensor_msgs::PointCloud2> ("pcl_output1", 1);
   pcl::PointCloud<pcl::PointXYZI> cloud;
   sensor_msgs::PointCloud2 output;
-  pcl::io::loadPCDFile ("/home/gray/Downloads/xiamen_port_map_part.pcd", cloud);//更换为自己的pcd文件路径
+  pcl::io::loadPCDFile (FLAGS_txt_path, cloud);//更换为自己的pcd文件路径
   
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_pcd(new pcl::PointCloud<pcl::PointXYZ>);
 
-	if (pcl::io::loadPCDFile("/home/gray/Downloads/xiamen_port_map_part.pcd", *cloud_pcd) == -1) {
+	if (pcl::io::loadPCDFile(FLAGS_txt_path, *cloud_pcd) == -1) {
 		return -1;
 	}
   //Convert the cloud to ROS message
@@ -38,11 +47,15 @@ main (int argc, char **argv)
   	//创建viewer对象
 	pcl::visualization::CloudViewer viewer("Cloud Viewer");
 	viewer.showCloud(cloud_pcd);
+  pcl::PointXYZ min_pt,max_pt;
+  pcl::getMinMax3D(*cloud_pcd, min_pt, max_pt);
 	// viewer.runOnVisualizationThreadOnce(viewerOneOff);
 
   ros::Rate loop_rate(1);
   while (ros::ok())
   {
+    // 循环打印min_pt和max_pt
+    std::cout<<"min_pt :"<<min_pt<<"//-----//  max_pt :"<<max_pt << std::endl;
     ros::spinOnce();
     loop_rate.sleep();
   }
